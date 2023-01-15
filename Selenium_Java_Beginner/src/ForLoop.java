c6db1c4b678f25c4c01665c93761d49b210bd563
Mateusz Czapla <czapla.mateusz.90@gmail.com>
2023-01-15 14:01:15 +0100

    Add ForLoop v3

--- Selenium_Java_Beginner/src/ForLoop.java
+++ Selenium_Java_Beginner/src/ForLoop.java
@@ -3,15 +3,15 @@ public class ForLoop {
     public static void main(String[] args) {
 
         // i = specjalna zmienna sterująca
-        /*for (int i = 0; i <= 100; i = i + 2) {
+        for (int i = 0; i <= 100; i = i + 2) {
             System.out.println(i);
-        }*/
+        }
 
-        /*for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++) {
             if (i % 5 == 0) {
                 System.out.println(i);
             }
-        }*/
+        }
         for (int sth = 0; sth < 20; sth++) {
             System.out.println("Nie będę marnował mojego cennego czasu na pierdoły.");
         }
